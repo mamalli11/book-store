e0accba1abf5be096198714cc7154f1c1f24eb73
Mohammad Javad <iman7260@gmail.com>
2024-07-17 02:51:41 +0330

    feat: Add Swagger and TypeORM configuration files

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -3,6 +3,7 @@ import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
 import { TypeOrmModule } from "@nestjs/typeorm";
 
+import { TypeOrmConfig } from "src/config/typeorm.config";
 
 @Module({
 	imports: [
