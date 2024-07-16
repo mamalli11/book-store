e0f1a9caf553f6546915c3c32c50ec61396e43a8
Mohammad Javad <iman7260@gmail.com>
2024-07-17 02:51:51 +0330

    feat: Add BaseEntity class for common entity functionality

---
+++ src/common/types/env.d.ts
@@ -0,0 +1,19 @@
+namespace NodeJS {
+	interface ProcessEnv {
+		//Application
+		PORT: number;
+		URL: string;
+		//Database
+		DB_PORT: number;
+		DB_NAME: string;
+		DB_USERNAME: string;
+		DB_PASSWORD: string;
+		DB_HOST: string;
+		//secrets
+		COOKIE_SECRET: string;
+		OTP_TOKEN_SECRET: string;
+		ACCESS_TOKEN_SECRET: string;
+		EMAIL_TOKEN_SECRET: string
+        PHONE_TOKEN_SECRET: string
+	}
+}
