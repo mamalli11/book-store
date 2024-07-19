85cb4ac521b6d83ecc509d285fb27132f9ca1e98
Mohammad Javad <iman7260@gmail.com>
2024-07-20 02:31:08 +0330

    chore: Add AuthModule and UserModule to app.module.ts

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -4,6 +4,8 @@ import { ConfigModule } from "@nestjs/config";
 import { TypeOrmModule } from "@nestjs/typeorm";
 
 import { TypeOrmConfig } from "src/config/typeorm.config";
+import { AuthModule } from "../auth/auth.module";
+import { UserModule } from "../user/user.module";
 
 @Module({
 	imports: [
@@ -12,7 +14,8 @@ import { TypeOrmConfig } from "src/config/typeorm.config";
 			envFilePath: join(process.cwd(), ".env"),
 		}),
 		TypeOrmModule.forRoot(TypeOrmConfig()),
-
+		AuthModule,
+		UserModule,
 	],
 	controllers: [],
 	providers: [],
---
+++ src/modules/user/enums/profile.enum.ts
@@ -0,0 +1,5 @@
+export enum GenderType {
+	Male = "male",
+	Other = "other",
+	Female = "female",
+}
