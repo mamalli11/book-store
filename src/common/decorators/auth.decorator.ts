6941f07b8da5fda8e2b661fd8e765ee9bf0d1c8d
Mohammad Javad <iman7260@gmail.com>
2024-07-22 02:57:52 +0330

    feat: Add AuthDecorator for applying authentication and role guards

---
+++ src/common/decorators/auth.decorator.ts
@@ -0,0 +1,8 @@
+import { applyDecorators, UseGuards } from "@nestjs/common";
+import { ApiBearerAuth } from "@nestjs/swagger";
+import { AuthGuard } from "src/modules/auth/guards/auth.guard";
+import { RoleGuard } from "src/modules/auth/guards/role.guard";
+
+export function AuthDecorator() {
+	return applyDecorators(ApiBearerAuth("Authorization"), UseGuards(AuthGuard, RoleGuard));
+}
