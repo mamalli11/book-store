1232dc9da28f78c490a0e963fda9802a1a275bd2
Mohammad Javad <iman7260@gmail.com>
2024-08-02 06:13:45 +0330

    install aws-sdk

--- src/common/types/env.d.ts
+++ src/common/types/env.d.ts
@@ -1,19 +1,29 @@
 namespace NodeJS {
 	interface ProcessEnv {
 		//Application
-		PORT: number;
 		URL: string;
+		PORT: number;
 		//Database
+		DB_HOST: string;
 		DB_PORT: number;
 		DB_NAME: string;
 		DB_USERNAME: string;
 		DB_PASSWORD: string;
-		DB_HOST: string;
 		//secrets
 		COOKIE_SECRET: string;
 		OTP_TOKEN_SECRET: string;
-		ACCESS_TOKEN_SECRET: string;
 		EMAIL_TOKEN_SECRET: string;
 		PHONE_TOKEN_SECRET: string;
+		ACCESS_TOKEN_SECRET: string;
+		//s3
+		S3_ENDPOINT: string;
+		S3_ACCESS_KEY: string;
+		S3_SECRET_KEY: string;
+		S3_BUCKET_NAME: string;
+		//Zarinnpal
+		ZARINPAL_VERIFY_URL: string;
+		ZARINPAL_REQUEST_URL: string;
+		ZARINPAL_GATEWAY_URL: string;
+		ZARINPAL_MERCHANT_ID: string;
 	}
 }
