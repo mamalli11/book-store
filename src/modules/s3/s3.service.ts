936b0bafab6372ddb7dae90cd27145cdd2042975
Mohammad Javad <iman7260@gmail.com>
2024-08-02 07:47:05 +0330

    Added S3 Service

---
+++ src/modules/s3/s3.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from "@nestjs/common";
+import { extname } from "path";
+import { S3 } from "aws-sdk";
+
+@Injectable()
+export class S3Service {
+	private readonly s3: S3;
+	constructor() {
+		this.s3 = new S3({
+			credentials: {
+				accessKeyId: process.env.S3_ACCESS_KEY,
+				secretAccessKey: process.env.S3_SECRET_KEY,
+			},
+			endpoint: process.env.S3_ENDPOINT,
+			region: "default",
+		});
+	}
+	async uploadFile(file: Express.Multer.File, folderName: string) {
+		const ext = extname(file.originalname);
+		return await this.s3
+			.upload({
+				Bucket: process.env.S3_BUCKET_NAME,
+				Key: `${folderName}/${Date.now()}${ext}`,
+				Body: file.buffer,
+			})
+			.promise();
+	}
+	async deleteFile(Key: string) {
+		return await this.s3
+			.deleteObject({
+				Bucket: process.env.S3_BUCKET_NAME,
+				Key: decodeURI(Key),
+			})
+			.promise();
+	}
+}
