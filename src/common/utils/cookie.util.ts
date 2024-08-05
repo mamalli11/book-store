648ef435860b6b092ad5f329b2120a44e5abcaa5
Mohammad Javad <iman7260@gmail.com>
2024-08-06 01:46:12 +0330

    change cookie config

--- src/common/utils/cookie.util.ts
+++ src/common/utils/cookie.util.ts
@@ -1,7 +1,10 @@
 export function CookiesOptionsToken() {
 	return {
 		secure: true,
+		signed: true,
 		httpOnly: true,
+		samesite: "lax",
+		maxAge: 1000 * 60 * 60 * 24 * 2, // would expire after 2 days
 		expires: new Date(Date.now() + 1000 * 60 * 2),
 	};
 }
