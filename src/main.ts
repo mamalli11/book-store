8c06eba7277e738d02eba7961f9eec117e6982c2
Mohammad Javad <iman7260@gmail.com>
2024-08-07 02:08:17 +0330

    change config cookie 5

--- src/main.ts
+++ src/main.ts
@@ -4,16 +4,36 @@ import { ValidationPipe } from "@nestjs/common";
 import { AppModule } from "./modules/app/app.module";
 import { SwaggerConfigInit } from "./config/swagger.config";
 import { NestExpressApplication } from "@nestjs/platform-express";
+import { NextFunction, Request, Response } from "express";
 
 async function bootstrap() {
-	const app = await NestFactory.create<NestExpressApplication>(AppModule);
-	const { PORT, NODE_ENV, COOKIE_SECRET, URL } = process.env;
-
-	app.enableCors({
-		origin: ["http://localhost:5173", "http://localhost:5174", "https://bookstoree.liara.run"],
-		credentials: true,
-		optionsSuccessStatus: 200,
+	const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+		cors: {
+			credentials: true,
+			origin: ["http://localhost:5173", "http://localhost:5174", "https://bookstoree.liara.run"],
+			optionsSuccessStatus: 200,
+		},
 	});
+	const { PORT, NODE_ENV, COOKIE_SECRET, URL } = process.env;
+	app.use((req: Request, res: Response, next: NextFunction) => {
+		const origin = req.headers.origin || "";
+		if (
+			[
+				"http://localhost:5173",
+				"http://localhost:5174",
+				"https://bookstoree.liara.run",
+			].includes(origin)
+		) {
+			res.header("Access-Control-Allow-Credentials", origin);
+		}
+		next();
+	});
+
+	// app.enableCors({
+	// 	origin: ["http://localhost:5173", "http://localhost:5174", "https://bookstoree.liara.run"],
+	// 	credentials: true,
+	// 	optionsSuccessStatus: 200,
+	// });
 
 	SwaggerConfigInit(app);
 	app.useStaticAssets("public");
