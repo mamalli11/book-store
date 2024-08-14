8a5f25768aacd2ce7a65451ca0877883fa41ec80
Mohammad Javad <iman7260@gmail.com>
2024-08-14 08:38:51 +0330

    install and use helmet, slugify

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import helmet from "helmet";
 import { NestFactory } from "@nestjs/core";
 import * as cookieParser from "cookie-parser";
 import { ValidationPipe } from "@nestjs/common";
@@ -6,15 +7,16 @@ import { SwaggerConfigInit } from "./config/swagger.config";
 import { NestExpressApplication } from "@nestjs/platform-express";
 
 async function bootstrap() {
-	const app = await NestFactory.create<NestExpressApplication>(AppModule, {
-		cors: {
-			credentials: true,
-			origin: ["http://localhost:5173", "http://localhost:5174", "https://bookstoree.liara.run"],
-			optionsSuccessStatus: 200,
-		},
-	});
-	
 	const { PORT, NODE_ENV, COOKIE_SECRET, URL } = process.env;
+
+	const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+	app.enableCors({
+		credentials: true,
+		origin: ["http://localhost:5173", "http://localhost:5174", "https://bookstoree.liara.run"],
+		optionsSuccessStatus: 200,
+	});
+	app.use(helmet());
 
 	SwaggerConfigInit(app);
 	app.useStaticAssets("public");
