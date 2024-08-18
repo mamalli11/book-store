a1b6c883b679c36b48160acfe94e62aac13c1a32
Mohammad Javad <iman7260@gmail.com>
2024-08-18 18:04:39 +0330

    domain remove

--- src/common/utils/cookie.util.ts
+++ src/common/utils/cookie.util.ts
@@ -6,7 +6,7 @@ export function CookiesOptionsToken(): CookieOptions {
 		secure: process.env.NODE_ENV === "production",
 		httpOnly: true,
 		sameSite: "lax",
-		domain: process.env.NODE_ENV === "production" ? "bookstoree.liara.run" : undefined,
+		// domain: process.env.NODE_ENV === "production" ? "bookstoree.liara.run" : undefined,
 		expires: new Date(Date.now() + 1000 * 60 * 60 * 24 * 2),
 		path: "/",
 	};
