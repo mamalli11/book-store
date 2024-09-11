534e059931ef4f4da028240b6757d15136c6b61a
Mohammad Javad <iman7260@gmail.com>
2024-09-11 17:32:20 +0330

    Refactor app module to include OrderModule and BasketModule

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -8,12 +8,14 @@ import { UserModule } from "../user/user.module";
 import { BooksModule } from "../books/books.module";
 import { EditorModule } from "../editor/editor.module";
 import { WriterModule } from "../writer/writer.module";
+import { BasketModule } from "../basket/basket.module";
 import { TypeOrmConfig } from "src/config/typeorm.config";
 import { CategoryModule } from "../category/category.module";
 import { DiscountModule } from "../discount/discount.module";
 import { CommentsModule } from "../comments/comments.module";
 import { PublisherModule } from "../publisher/publisher.module";
 import { TranslatorModule } from "../translator/translator.module";
+import { OrderModule } from "../order/order.module";
 
 @Module({
 	imports: [
@@ -24,9 +26,11 @@ import { TranslatorModule } from "../translator/translator.module";
 		TypeOrmModule.forRoot(TypeOrmConfig()),
 		AuthModule,
 		UserModule,
+		OrderModule,
 		BooksModule,
 		WriterModule,
 		EditorModule,
+		BasketModule,
 		CategoryModule,
 		DiscountModule,
 		CommentsModule,
