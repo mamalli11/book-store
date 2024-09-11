3684acfff31e0a0c1e1f8e97d40b453b21482c08
Mohammad Javad <iman7260@gmail.com>
2024-09-11 17:26:36 +0330

    Refactor basket module and add books and discount services

--- src/modules/basket/basket.module.ts
+++ src/modules/basket/basket.module.ts
@@ -3,13 +3,15 @@ import { TypeOrmModule } from "@nestjs/typeorm";
 
 import { BasketService } from "./basket.service";
 import { AuthModule } from "../auth/auth.module";
+import { BooksModule } from "../books/books.module";
 import { BasketController } from "./basket.controller";
 import { BasketEntity } from "./entities/basket.entity";
+import { DiscountService } from "../discount/discount.service";
 import { DiscountEntity } from "../discount/entities/discount.entity";
 
 @Module({
-	imports: [AuthModule, TypeOrmModule.forFeature([BasketEntity, DiscountEntity])],
+	imports: [AuthModule, BooksModule, TypeOrmModule.forFeature([BasketEntity, DiscountEntity])],
 	controllers: [BasketController],
-	providers: [BasketService],
+	providers: [BasketService, DiscountService],
 })
 export class BasketModule {}
