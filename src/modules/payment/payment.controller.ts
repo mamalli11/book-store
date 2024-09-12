434fac2b7204ff89788c67087bbd9f800b197358
Mohammad Javad <iman7260@gmail.com>
2024-09-13 02:21:28 +0330

    Refactor PaymentController to support multiple request content types

--- src/modules/payment/payment.controller.ts
+++ src/modules/payment/payment.controller.ts
@@ -1,10 +1,11 @@
 import { Response } from "express";
-import { ApiTags } from "@nestjs/swagger";
+import { ApiConsumes, ApiTags } from "@nestjs/swagger";
 import { Body, Controller, Get, Post, Query, Res } from "@nestjs/common";
 
 import { PaymentDto } from "./dto/payment.dto";
 import { PaymentService } from "./payment.service";
 import { AuthDecorator } from "src/common/decorators/auth.decorator";
+import { SwaggerConsumes } from "src/common/enums/swagger-consumes.enum";
 
 @Controller("Payment")
 @ApiTags("Payment")
@@ -13,6 +14,7 @@ export class PaymentController {
 	constructor(private paymentService: PaymentService) {}
 
 	@Post()
+	@ApiConsumes(SwaggerConsumes.UrlEncoded, SwaggerConsumes.Json)
 	gatewayUrl(@Body() paymentDto: PaymentDto) {
 		return this.paymentService.getGatewayUrl(paymentDto);
 	}
