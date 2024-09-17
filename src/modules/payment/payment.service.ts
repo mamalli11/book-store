7b28dfc30c2bd125d0d1c11569a1e0bc8678ade7
Mohammad Javad <iman7260@gmail.com>
2024-09-17 17:43:22 +0330

    Refactor PaymentService to include DiscountEntity and update payment functionality

--- src/modules/payment/payment.service.ts
+++ src/modules/payment/payment.service.ts
@@ -1,39 +1,50 @@
+import {
+	Scope,
+	Inject,
+	Injectable,
+	ConflictException,
+	NotFoundException,
+	BadRequestException,
+} from "@nestjs/common";
 import { Request } from "express";
+import { randomInt } from "crypto";
 import { Repository } from "typeorm";
 import { REQUEST } from "@nestjs/core";
 import { InjectRepository } from "@nestjs/typeorm";
-import { Scope, Inject, Injectable, ConflictException, NotFoundException } from "@nestjs/common";
 
+import { PaymentDto } from "./dto/payment.dto";
 import { OrderService } from "../order/order.service";
-import { OrderStatus } from "../order/enum/status.enum";
 import { BasketService } from "../basket/basket.service";
 import { PaymentEntity } from "./entities/payment.entity";
 import { ZarinpalService } from "../http/zarinpal.service";
-import { PaymentDataDto, PaymentDto } from "./dto/payment.dto";
+import { DiscountEntity } from "../discount/entities/discount.entity";
+import { OrderItemStatus, OrderStatus } from "../order/enum/status.enum";
 
 @Injectable({ scope: Scope.REQUEST })
 export class PaymentService {
 	constructor(
+		@InjectRepository(PaymentEntity) private paymentRepository: Repository<PaymentEntity>,
+		@InjectRepository(DiscountEntity) private discountRepository: Repository<DiscountEntity>,
 		@Inject(REQUEST) private req: Request,
+		private orderService: OrderService,
 		private basketService: BasketService,
 		private zarinpalService: ZarinpalService,
-		private orderService: OrderService,
-		@InjectRepository(PaymentEntity)
-		private paymentRepository: Repository<PaymentEntity>,
 	) {}
 
 	async getGatewayUrl(paymentDto: PaymentDto) {
 		const { id: userId, email, phone } = this.req.user;
+
 		const basket = await this.basketService.getBasket();
 		const order = await this.orderService.create(basket, paymentDto);
-		const payment = await this.create({
-			amount: basket.payment_amount,
-			orderId: order.id,
-			status: basket.payment_amount === 0,
+		const payment = this.paymentRepository.create({
 			userId,
-			invoice_number: new Date().getTime().toString(),
+			orderId: order.id,
+			amount: basket.payment_amount,
+			status: basket.payment_amount === 0,
+			invoice_number: new Date().getTime().toString() + randomInt(10000, 99999).toString(),
 		});
-		if (payment.status) {
+
+		if (!payment.status) {
 			const { authority, code, gatewayURL } = await this.zarinpalService.sendRequest({
 				amount: basket.payment_amount,
 				description: "PAYMENT ORDER",
@@ -43,36 +54,26 @@ export class PaymentService {
 			await this.paymentRepository.save(payment);
 			return { gatewayURL, code };
 		}
-		return {
-			message: "payment successfully",
-		};
-	}
-
-	async create(paymentDto: PaymentDataDto) {
-		const { amount, invoice_number, orderId, status, userId } = paymentDto;
-		const payment = this.paymentRepository.create({
-			amount,
-			status,
-			userId,
-			orderId,
-			invoice_number,
-		});
-		return await this.paymentRepository.save(payment);
+		return { message: "payment successfully" };
 	}
 
 	async verify(authority: string, status: string) {
 		const payment = await this.paymentRepository.findOneBy({ authority });
+
 		if (!payment) throw new NotFoundException();
 		if (payment.status) throw new ConflictException("already verified");
 		if (status === "OK") {
+			await this.basketService.getBasketDiscount(payment.userId);
 			const order = await this.orderService.findOne(payment.orderId);
 			order.status = OrderStatus.Paid;
 			await this.orderService.save(order);
+			await this.orderService.updateOrderItem(order.id, OrderItemStatus.Sent);
+			await this.basketService.basketDisable(order.userId);
 			payment.status = true;
 		} else {
-			return "http://frontendurl.com/payment?status=failed";
+			throw new BadRequestException("Payment failed");
 		}
 		await this.paymentRepository.save(payment);
-		return "http://frontendurl.com/payment?status=success";
+		return { message: "Payment was successful" };
 	}
 }
