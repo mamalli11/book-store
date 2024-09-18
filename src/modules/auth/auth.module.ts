22faa5d444edc5bc51bfef1629beeb5fc6fbd388
Mohammad Javad <iman7260@gmail.com>
2024-09-18 16:39:53 +0330

    Refactor AuthModule to include HttpApiModule for sending welcome emails

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -5,15 +5,15 @@ import { TypeOrmModule } from "@nestjs/typeorm";
 import { AuthService } from "./auth.service";
 import { TokenService } from "./tokens.service";
 import { AuthController } from "./auth.controller";
-import { MailService } from "../http/mail.service";
+import { HttpApiModule } from "../http/http.module";
 import { OtpEntity } from "../user/entities/otp.entity";
 import { UserEntity } from "../user/entities/user.entity";
 import { ProfileEntity } from "../user/entities/profile.entity";
 
 @Module({
-	imports: [TypeOrmModule.forFeature([UserEntity, OtpEntity, ProfileEntity])],
+	imports: [HttpApiModule, TypeOrmModule.forFeature([UserEntity, OtpEntity, ProfileEntity])],
 	controllers: [AuthController],
-	providers: [AuthService, JwtService, TokenService, MailService],
+	providers: [AuthService, JwtService, TokenService],
 	exports: [AuthService, JwtService, TokenService],
 })
 export class AuthModule {}
