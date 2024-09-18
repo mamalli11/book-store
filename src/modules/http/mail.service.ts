17363c8c3b67139a30686c6264cec135c028d0be
Mohammad Javad <iman7260@gmail.com>
2024-09-18 16:48:07 +0330

    Refactor MailService to use setImmediate for sending emails asynchronously

--- src/modules/http/mail.service.ts
+++ src/modules/http/mail.service.ts
@@ -6,7 +6,7 @@ export class MailService {
 	constructor(private mailService: MailerService) {}
 
 	async welcomMail(email: string, fullName: string = "کاربر") {
-		try {
+		setImmediate(async () => {
 			await this.mailService.sendMail({
 				to: email,
 				from: `welcome <${process.env.MAIL_FROM}>`,
@@ -17,8 +17,29 @@ export class MailService {
 					fullName,
 				},
 			});
-		} catch (error) {
-			console.log(error);
-		}
+		});
+	}
+
+	async verificationMail(email: string, otp_code: string) {
+		setImmediate(async () => {
+			await this.mailService.sendMail({
+				to: email,
+				from: `verification <${process.env.MAIL_FROM}>`,
+				subject: "Verification Code",
+				template: "verification.html",
+				text: `
+				سلام،
+				
+				کد تأیید شما برای ورود به سیستم: ${otp_code}
+
+				این کد به مدت ۲ دقیقه معتبر است. لطفاً از آن برای ورود به حساب کاربری خود استفاده کنید.
+
+				اگر شما این درخواست را نساخته‌اید، لطفاً این ایمیل را نادیده بگیرید.
+				`,
+				context: {
+					otp_code,
+				},
+			});
+		});
 	}
 }
