0a71639c79f770b443c6c9b0caefbcc15d9049dd
Mohammad Javad <iman7260@gmail.com>
2024-09-21 01:45:30 +0330

    Refactor AuthGuard to support internationalization for login validation messages

--- src/modules/auth/guards/auth.guard.ts
+++ src/modules/auth/guards/auth.guard.ts
@@ -1,17 +1,18 @@
 import { Request } from "express";
 import { isJWT } from "class-validator";
 import { Reflector } from "@nestjs/core";
+import { I18nService, I18nContext } from "nestjs-i18n";
 import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from "@nestjs/common";
 
 import { AuthService } from "../auth.service";
-import { AuthMessage } from "src/common/enums/message.enum";
 import { SKIP_AUTH } from "src/common/decorators/skip-auth.decorator";
 
 @Injectable()
 export class AuthGuard implements CanActivate {
 	constructor(
-		private authService: AuthService,
 		private reflector: Reflector,
+		private authService: AuthService,
+		private readonly i18n: I18nService,
 	) {}
 	async canActivate(context: ExecutionContext) {
 		const isSkippedAuthorization = this.reflector.get<boolean>(SKIP_AUTH, context.getHandler());
@@ -25,11 +26,19 @@ export class AuthGuard implements CanActivate {
 	protected extractToken(request: Request) {
 		const { authorization } = request.headers;
 		if (!authorization || authorization?.trim() == "") {
-			throw new UnauthorizedException(AuthMessage.LoginIsRequired);
+			throw new UnauthorizedException(
+				this.i18n.t("tr.AuthMessage.LoginIsRequired", {
+					lang: I18nContext.current().lang,
+				}),
+			);
 		}
 		const [bearer, token] = authorization?.split(" ");
 		if (bearer?.toLowerCase() !== "bearer" || !token || !isJWT(token)) {
-			throw new UnauthorizedException(AuthMessage.LoginIsRequired);
+			throw new UnauthorizedException(
+				this.i18n.t("tr.AuthMessage.LoginIsRequired", {
+					lang: I18nContext.current().lang,
+				}),
+			);
 		}
 		return token;
 	}
