e0be74554e65462b7607a158da45a13041049880
Mohammad Javad <iman7260@gmail.com>
2024-09-20 23:53:59 +0330

    Refactor DiscountService to support internationalization

--- src/modules/discount/discount.service.ts
+++ src/modules/discount/discount.service.ts
@@ -6,17 +6,18 @@ import {
 } from "@nestjs/common";
 import { DeepPartial, Repository } from "typeorm";
 import { InjectRepository } from "@nestjs/typeorm";
+import { I18nService, I18nContext } from "nestjs-i18n";
 
 import { DiscountEntity } from "./entities/discount.entity";
 import { CreateDiscountDto } from "./dto/create-discount.dto";
 import { PaginationDto } from "src/common/dtos/pagination.dto";
-import { NotFoundMessage, PublicMessage } from "src/common/enums/message.enum";
 import { paginationGenerator, paginationSolver } from "src/common/utils/pagination.util";
 
 @Injectable()
 export class DiscountService {
 	constructor(
 		@InjectRepository(DiscountEntity) private discountRepository: Repository<DiscountEntity>,
+		private readonly i18n: I18nService,
 	) {}
 
 	async create(createDiscountDto: CreateDiscountDto) {
@@ -25,7 +26,11 @@ export class DiscountService {
 
 		const discountObject: DeepPartial<DiscountEntity> = { code };
 		if ((!amount && !percent) || (amount && percent)) {
-			throw new BadRequestException("You must enter one of the amount or percent fields ");
+			throw new BadRequestException(
+				this.i18n.t("tr.BadRequestMessage.AmountOrPercent", {
+					lang: I18nContext.current().lang,
+				}),
+			);
 		}
 		if (amount && !isNaN(parseFloat(amount.toString()))) {
 			discountObject["amount"] = amount;
@@ -47,7 +52,11 @@ export class DiscountService {
 			type,
 		});
 		await this.discountRepository.save(discount);
-		return { message: PublicMessage.CreatedDiscount };
+		return {
+			message: this.i18n.t("tr.PublicMessage.CreatedDiscount", {
+				lang: I18nContext.current().lang,
+			}),
+		};
 	}
 
 	async findAll(paginationDto: PaginationDto) {
@@ -66,30 +75,52 @@ export class DiscountService {
 
 	async findOne(id: number) {
 		const discount = await this.discountRepository.findOneBy({ id });
-		if (!discount) throw new NotFoundException(NotFoundMessage.NotFoundDiscount);
+		if (!discount)
+			throw new NotFoundException(
+				this.i18n.t("tr.NotFoundMessage.NotFoundDiscount", {
+					lang: I18nContext.current().lang,
+				}),
+			);
 		return discount;
 	}
 
 	async delete(id: number) {
 		await this.findOne(id);
 		await this.discountRepository.delete({ id });
-		return { message: PublicMessage.Deleted };
+		return {
+			message: this.i18n.t("tr.PublicMessage.Deleted", { lang: I18nContext.current().lang }),
+		};
 	}
 
 	async checkExistCode(code: string) {
 		const discount = await this.discountRepository.findOneBy({ code });
-		if (discount) throw new ConflictException("already exist code");
+		if (discount)
+			throw new ConflictException(
+				this.i18n.t("tr.ConflictMessage.AlreadyExistDiscountCode", {
+					lang: I18nContext.current().lang,
+				}),
+			);
 	}
 
 	async findOneByCode(code: string) {
 		const discount = await this.discountRepository.findOneBy({ code });
-		if (!discount) throw new NotFoundException("not found discount code");
+		if (!discount)
+			throw new NotFoundException(
+				this.i18n.t("tr.NotFoundMessage.NotFoundDiscount", {
+					lang: I18nContext.current().lang,
+				}),
+			);
 		return discount;
 	}
 
 	async useDiscount(code: string) {
 		const discount = await this.findOneByCode(code);
-		if (!discount.active) throw new BadRequestException("not active discount");
+		if (!discount.active)
+			throw new BadRequestException(
+				this.i18n.t("tr.BasketMessage.NotActiveDiscount", {
+					lang: I18nContext.current().lang,
+				}),
+			);
 
 		discount.usage += 1;
 		if (discount.limit !== null && discount.usage >= discount.limit) {
