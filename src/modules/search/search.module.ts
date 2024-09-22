3f8648b8edb4cee5ed8e710dd4e7128fc7b38fdc
Mohammad Javad <iman7260@gmail.com>
2024-09-22 23:41:27 +0330

    Refactor search module imports and add FrequentSearchEntity

--- src/modules/search/search.module.ts
+++ src/modules/search/search.module.ts
@@ -7,12 +7,20 @@ import { SearchController } from "./search.controller";
 import { WriterEntity } from "../writer/entities/writer.entity";
 import { EditorEntity } from "../editor/entities/editor.entity";
 import { PublisherEntity } from "../publisher/entities/publisher.entity";
+import { FrequentSearchEntity } from "./entities/FrequentSearches.entity";
 import { TranslatorEntity } from "../translator/entities/translator.entity";
+``;
 
 @Module({
 	imports: [
 		BooksModule,
-		TypeOrmModule.forFeature([TranslatorEntity, WriterEntity, PublisherEntity, EditorEntity]),
+		TypeOrmModule.forFeature([
+			EditorEntity,
+			WriterEntity,
+			PublisherEntity,
+			TranslatorEntity,
+			FrequentSearchEntity,
+		]),
 	],
 	controllers: [SearchController],
 	providers: [SearchService],
