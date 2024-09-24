d3f62f92aec8f4c14b8f1e011e06ea2fd4ce678c
Mohammad Javad <iman7260@gmail.com>
2024-09-24 08:18:51 +0330

    Refactor discount.module.ts to import OrderEntity and add it to the TypeOrmModule imports

--- src/modules/discount/discount.module.ts
+++ src/modules/discount/discount.module.ts
@@ -5,9 +5,10 @@ import { AuthModule } from "../auth/auth.module";
 import { DiscountService } from "./discount.service";
 import { DiscountController } from "./discount.controller";
 import { DiscountEntity } from "./entities/discount.entity";
+import { OrderEntity } from "../order/entities/order.entity";
 
 @Module({
-	imports: [AuthModule, TypeOrmModule.forFeature([DiscountEntity])],
+	imports: [AuthModule, TypeOrmModule.forFeature([DiscountEntity, OrderEntity])],
 	controllers: [DiscountController],
 	providers: [DiscountService],
 })
