a7c679b16ee75af97937d24d19ada5604ce25a09
Mohammad Javad <iman7260@gmail.com>
2024-09-24 07:06:01 +0330

    Refactor upload-file.interceptor.ts to import memoryStorage from multer

--- src/common/interceptors/upload-file.interceptor.ts
+++ src/common/interceptors/upload-file.interceptor.ts
@@ -1,6 +1,6 @@
+import { memoryStorage } from "multer";
 import { FileFieldsInterceptor, FileInterceptor } from "@nestjs/platform-express";
 import { MulterField } from "@nestjs/platform-express/multer/interfaces/multer-options.interface";
-import { memoryStorage } from "multer";
 
 export function UploadFileS3(filedName: string) {
 	return class UploadUtility extends FileInterceptor(filedName, {
